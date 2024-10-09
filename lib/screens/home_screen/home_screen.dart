aac0fde7cb857803cbd9437f9c170fd36952579c
Eduardo F Costa <e.feitosa@spotpromo.com.br>
2024-10-08 21:16:57 -0300

    implemtando interceptors para fins de controle com log de requisicoes

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -29,6 +29,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     refresh();
+    JournalService().get();
     super.initState();
   }
 
--- lib/services/journal_service.dart
+++ lib/services/journal_service.dart
@@ -1,19 +1,24 @@
+import 'package:flutter_webapi_first_course/services/http_interceptors.dart';
 import 'package:http/http.dart' as http;
+import 'package:http_interceptor/http_interceptor.dart';
 
 class JournalService {
   static const String url = "https://adidas.spotpromo.com.br/api/";
   static const String resources = "flutter_api.php";
+
+  final http.Client _client =
+      InterceptedClient.build(interceptors: [LoggerInterceptor()]);
 
   String _getUrl() {
     return "$url$resources";
   }
 
   Future<String> get() async {
-     http.Response response = await http.get(Uri.parse(_getUrl()));
-     return response.body;
+    http.Response response = await _client.get(Uri.parse(_getUrl()));
+    return response.body;
   }
 
   register(String content) {
-    http.post(Uri.parse(_getUrl()), body: { "content" : content });
+    _client.post(Uri.parse(_getUrl()), body: {"content": content});
   }
 }
